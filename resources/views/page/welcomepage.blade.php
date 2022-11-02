4d5b652d654506558b16b43c6369c5d3ba5831f6
ThanhHungA33367 <a33367@thanglong.edu.vn>
2022-11-02 22:50:28 +0700

    update layout
    update layout

---
+++ resources/views/page/welcomepage.blade.php
@@ -0,0 +1,4 @@
+@extends('layout.master')
+@section('content')
+    Day la trang chu
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('page.welcomepage');
 });
 Route::get('/layout', function () {
     return view('layout');
