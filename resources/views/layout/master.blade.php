e904e57715392acde575bf322381e6eeac901fe6
namtrong01 <namlaitrong01@gmail.com>
2022-11-04 20:29:49 +0700

    k

---
+++ resources/views/layout/master.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Starter | Hyper - Responsive Bootstrap 4 Admin Dashboard</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
+    <meta content="Coderthemes" name="author" />
+    <!-- App favicon -->
+
+
+    <!-- App css -->
+
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link href="{{ asset('css/app-creative.min.css') }}" rel="stylesheet" type="text/css" id="light-style"/>
+{{--    <link href="{{ asset('css/now-ui-kit.css_v=1.1.0.css') }}" rel="stylesheet" type="text/css"/>--}}
+    <link href="{{ asset('css/icons.min.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" type="text/css"/>
+    <link href="{{ asset('css/app-creative-dark.min.css') }}" rel="stylesheet" type="text/css" id="dark-style"/>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+
+</head>
+
+<body class="loading" data-layout="topnav" data-layout-config='{"layoutBoxed":false,"darkMode":false,"showRightSidebarOnStart": true}'>
+
+<!-- Begin page -->
+<div class="wrapper">
+    <!-- ============================================================== -->
+    <!-- Start Page Content here -->
+    <!-- ============================================================== -->
+    <div class="content-page">
+        <div class="content">
+            <!-- Topbar Start -->
+           @include('layout.header')
+            <!-- end Topbar -->
+
+            <!-- Start Content-->
+            <div class="container-fluid">
+
+                <!-- start page title -->
+                <div class="row">
+                    <div class="col-12">
+                        <div class="page-title-box">
+                            <h4 class="page-title">Starter</h4>
+                        </div>
+                        @yield('content')
+                    </div>
+                </div>
+                <!-- end page title -->
+
+            </div>
+            <!-- container -->
+
+        </div>
+        <!-- content -->
+        <!-- Footer Start -->
+        @include('layout.footer')
+        <!-- end Footer -->
+    </div>
+    <!-- ============================================================== -->
+    <!-- End Page content -->
+    <!-- ============================================================== -->
+</div>
+<!-- END wrapper -->
+<!-- Right Sidebar -->
+@include('layout.rightsidebar')
+<!-- /Right-bar -->
+<!-- bundle -->
+
+<script src="{{ asset('js/vendor.min.js') }}"></script>
+<script src="{{ asset('js/app.min.js') }}"></script>
+</body>
+
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('./views/lauouts/master');
+    return view('home');
 });
 
